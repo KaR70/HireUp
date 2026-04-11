2f75233b115eea2f125f0f60a5279ccd6558770a
Kareem Osama <kareemosama409@gmail.com>
2026-04-11 03:50:21 +0200

    Fixed Git Conflict

--- HireUp-Api/HireUp/Entities/Company.cs
+++ HireUp-Api/HireUp/Entities/Company.cs
@@ -4,7 +4,7 @@ public class Company
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public string Logo { get; set; }
 
     public virtual ICollection<JobListing> JobListings{ get; set; }
